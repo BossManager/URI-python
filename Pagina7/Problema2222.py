a1d17bbd9c759511193b78cb0e59e52ddeea40fe
Daniel Nogueira de Oliveira <dan.engine@hotmail.com>
2018-09-02 09:01:21 -0300

    Mais tentativas de otimizaçao

--- Pagina7/Problema2222.py
+++ Pagina7/Problema2222.py
@@ -2,11 +2,9 @@
 for i in range(int(input())):
     conjuntos = []
     for j in range(int(input())):
-        itens = input().split()
-        conjuntos.append(itens[1:])
+        conjuntos.append(input().split()[1:])
     for p in range(int(input())):
-        lista = map(int,input().split())
-        n1,n2,n3 = lista
+        n1,n2,n3 = map(int,input().split())
         if n1==1:
             print(len(set(conjuntos[n2-1]) & set(conjuntos[n3-1])))
         else:
